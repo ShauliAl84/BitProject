bd7220300143a20348d6bca4213a4abe22cf4533
ShauliAlgawi <160117907+ShauliAlgawi@users.noreply.github.com>
2025-02-09 12:16:14 +0200

    Last fixes
    Added missing packages. Fixed an issue with closing the Trailer view sheet.

--- BitProject/Views/TrailerVideoContainer.swift
+++ BitProject/Views/TrailerVideoContainer.swift
@@ -10,21 +10,41 @@ import YouTubePlayerKit
 
 struct TrailerVideoContainer: View {
     let youTubePlayer: YouTubePlayer
+    @Environment(\.dismiss) private var dismiss
     
     init(videoId: String) {
         self.youTubePlayer = YouTubePlayer(urlString: "https://youtube.com/watch?v=\(videoId)")
     }
     
     var body: some View {
-        YouTubePlayerView(youTubePlayer) { state in
-            switch state {
-            case .idle:
-                ProgressView()
-            case .ready:
-                EmptyView()
-            case .error(let error):
-                ContentUnavailableView("Error", systemImage: "exclamationmark.triangle.fill", description: Text("YouTube player couldn't be loaded: \(error)"))
+        NavigationStack {
+            VStack {
+                YouTubePlayerView(youTubePlayer) { state in
+                    switch state {
+                    case .idle:
+                        ProgressView()
+                    case .ready:
+                        EmptyView()
+                    case .error(let error):
+                        ContentUnavailableView("Error", systemImage: "exclamationmark.triangle.fill", description: Text("YouTube player couldn't be loaded: \(error)"))
+                    }
+                }
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        withAnimation {
+                            dismiss()
+                        }
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundStyle(Color.gray)
+                    }
+
+                }
+            }
+            .presentationDragIndicator(.hidden)
+            .interactiveDismissDisabled(true)
         }
     }
 }
